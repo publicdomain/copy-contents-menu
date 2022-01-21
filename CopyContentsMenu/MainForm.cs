4eed94d91e0b1afc03ac876dd5512c691bed7b7f
Victor <publicdomainvic@gmail.com>
2022-01-20 20:51:42 -0400

    Add OnExitToolStripMenuItemClick()

--- CopyContentsMenu/MainForm.cs
+++ CopyContentsMenu/MainForm.cs
@@ -6,50 +6,56 @@ using System.Windows.Forms;
 
 namespace CopyContentsMenu
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			this.InitializeComponent();
-		}
-		
-		private void OnAddButtonClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		private void OnRemoveButtonClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-	}
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+        }
+
+        private void OnAddButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnRemoveButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the exit tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // Close application
+            this.Close();
+        }
+    }
 }
