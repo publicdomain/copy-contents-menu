0725fb42ab649b40a3f22067247b3cd4d5413142
Victor <publicdomainvic@gmail.com>
2022-01-21 03:06:35 -0400

    Update assembly info

--- CopyContentsMenu/Properties/AssemblyInfo.cs
+++ CopyContentsMenu/Properties/AssemblyInfo.cs
@@ -10,12 +10,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CopyContentsMenu")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds \"Copy contents\" to Windows Explorer's context menu.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PublicDomain.is")]
 [assembly: AssemblyProduct("CopyContentsMenu")]
-[assembly: AssemblyCopyright("Copyright 2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright 2022 - Victor L. Senior")]
+[assembly: AssemblyTrademark("publicdomain")]
 [assembly: AssemblyCulture("")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
