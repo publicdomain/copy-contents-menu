0708cfe9f1de9f1b0a443321153e785bd1a5f601
Victor <publicdomainvic@gmail.com>
2022-01-20 20:43:35 -0400

    Add project files

---
+++ CopyContentsMenu/MainForm.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CopyContentsMenu
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			this.InitializeComponent();
+		}
+		
+		private void OnAddButtonClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		private void OnRemoveButtonClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
